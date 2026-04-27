bad418be04072e8e126df153d3a0b7d76b3377fe
PreciousArielle <202410630@gordoncollege.edu.ph>
2026-04-27 13:18:30 +0800

    fixed db to allow render

--- BACKEND/db.js
+++ BACKEND/db.js
@@ -2,13 +2,23 @@ require('dotenv').config()
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-  host:     process.env.DB_HOST     || 'localhost',
-  user:     process.env.DB_USER     || 'root',
+  // This looks at Render first, then uses localhost if Render isn't there
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME     || 'profile_db',
+  database: process.env.DB_NAME || 'profile_db',
+  
+
+  port: process.env.DB_PORT || 3306, 
+
+
+  ssl: {
+    rejectUnauthorized: false
+  },
+
   waitForConnections: true,
-  connectionLimit:    10,
-  queueLimit:         0
+  connectionLimit: 10,
+  queueLimit: 0
 })
 
 // Test the connection on startup
